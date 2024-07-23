ce5ba54476882d9aac954e8a002fe63bb2d2969d
ottowillborn <ottowillborn@gmail.com>
2024-07-22 21:24:41 -0700

    Initial Commit

---
+++ TalkALot/TalkALotApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkALotApp.swift
+//  TalkALot
+//
+//  Created by Otto Willborn on 2024-07-22.
+//
+
+import SwiftUI
+
+@main
+struct TalkALotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
